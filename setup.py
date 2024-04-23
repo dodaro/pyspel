6f7acc8089b25978446846c48b3991121f951b9a
dodaro <dodaro@mat.unical.it>
2024-04-23 21:57:58 +0200

    Modified version.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 NAME = 'pyspel'
 DESCRIPTION = 'Python Specification Language (Pyspel)'
 LONG_DESCRIPTION = 'Pyspel is a python-based specification language that combines python programs with Answer Set Programming'
@@ -8,14 +8,14 @@ LONG_DESCRIPTION = 'Pyspel is a python-based specification language that combine
 setup(
   name=NAME,
   packages=find_packages(),
-  version='1.0.0',
+  version='1.0.1',
   license='Apache 2.0',
   description=DESCRIPTION,
   long_description=LONG_DESCRIPTION,
   author='Carmine Dodaro',
   author_email='carmine.dodaro@unical.it',
   url='https://github.com/dodaro/pyspel',
-  download_url='https://github.com/dodaro/pyspel/archive/refs/tags/v1.0.0.tar.gz',
+  download_url='https://github.com/dodaro/pyspel/archive/refs/tags/v1.0.1.tar.gz',
   keywords=['answer set programming', 'specification language', 'combinatorial problems'],
   install_requires=['decorator'],
   classifiers=[
